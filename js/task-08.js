0f7681a5c1afd47c7d7a3f517caa60afc05247b1
Volodymyr Poliakov <vladimir03002@gmail.com>
2023-02-11 23:07:57 +0100

    have done eighth task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,47 @@
+//!
+// Напиши скрипт управління формою логіна.
 
+// <form class="login-form">
+//   <label>
+//     Email
+//     <input type="email" name="email" />
+//   </label>
+//   <label>
+//     Password
+//     <input type="password" name="password" />
+//   </label>
+//   <button type="submit">Login</button>
+// </form>
+
+// Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
+// Під час відправлення форми сторінка не повинна перезавантажуватися.
+// Якщо у формі є незаповнені поля, виводь alert з попередженням про те,
+// що всі поля повинні бути заповнені.
+// Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт,
+// де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості.
+// Для доступу до елементів форми використовуй властивість elements.
+// Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+
+function createObjectFromSubmitButtonForm() {
+  const formEmailPassword = document.querySelector(".login-form");
+
+  formEmailPassword.addEventListener("submit", btnSubmitEventListener);
+
+  function btnSubmitEventListener(event) {
+    event.preventDefault();
+
+    const {
+      elements: { email, password },
+    } = event.currentTarget;
+
+    email.value === "" || password.value === ""
+      ? alert("All form fields must be filled out!")
+      : false;
+
+    console.log(`Login: ${email.value}, Password: ${password.value}`);
+
+    event.currentTarget.reset();
+  }
+}
+
+createObjectFromSubmitButtonForm();
